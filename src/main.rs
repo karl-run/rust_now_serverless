14ec81d027eb5721985ca87fc9c402f5b27d5bb3
Karl J. Overå <k@rl.run>
2018-11-05 22:39:12 +0100

    Add basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+#[macro_use]
+extern crate rouille;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Now listening on localhost:5000");
+
+    rouille::start_server("localhost:5000", move |request| {
+        router!(request,
+            (GET) (/secret) => {
+                rouille::Response::text(format!("Welcome to the secret"))
+            },
+            (GET) (/{path: String}) => {
+                rouille::Response::text(format!("Hello path: {}", path))
+            },
+            _ => rouille::Response::empty_404()
+        )
+    });
 }
