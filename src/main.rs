6d364f82d2919b58cdb5dbca15aed36c2a8eb2a6
Karl J. Overå <k@rl.run>
2018-11-06 11:41:09 +0100

    Fix so that now sees the port

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@
 extern crate rouille;
 
 fn main() {
-    println!("Now listening on localhost:5000");
+    println!("Now listening on port 5000");
 
-    rouille::start_server("localhost:5000", move |request| {
+    rouille::start_server("0.0.0.0:5000", move |request| {
         router!(request,
             (GET) (/secret) => {
                 rouille::Response::text(format!("Welcome to the secret"))
